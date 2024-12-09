ee4afcbb187ca3c64b46e418b341a2b017eb35c0
George Kasparyants <gg_plastt@mail.ru>
2024-12-09 05:47:21 +0300

    extract script from result

--- most/types.py
+++ most/types.py
@@ -27,15 +27,6 @@ class ColumnResult(DataClassJsonMixin):
 
 @dataclass_json
 @dataclass
-class Result(DataClassJsonMixin):
-    id: str
-    text: Optional[str]
-    url: Optional[str]
-    results: Optional[List[ColumnResult]]
-
-
-@dataclass_json
-@dataclass
 class Column(DataClassJsonMixin):
     name: str
     subcolumns: List[str]
@@ -51,3 +42,21 @@ class Script(DataClassJsonMixin):
 @dataclass
 class JobStatus(DataClassJsonMixin):
     status: Literal["not_found", "pending", "completed", "error"]
+
+
+@dataclass_json
+@dataclass
+class Result(DataClassJsonMixin):
+    id: str
+    text: Optional[str]
+    url: Optional[str]
+    results: Optional[List[ColumnResult]]
+
+    def get_script(self) -> Optional[Script]:
+        if self.results is None:
+            return None
+
+        return Script(columns=[Column(name=column_result.name,
+                                      subcolumns=[subcolumn_result.name
+                                                  for subcolumn_result in column_result.subcolumns])
+                               for column_result in self.results])
