28bbd5c2111759931dc0afa5d56384a5ea3d922b
George Kasparyants <gg_plastt@mail.ru>
2025-02-26 11:52:52 +0300

    fix bson usage in code

--- most/types.py
+++ most/types.py
@@ -1,6 +1,5 @@
+import re
 from dataclasses import dataclass
-
-from bson import ObjectId
 from dataclasses_json import dataclass_json, DataClassJsonMixin
 from typing import Optional, List, Literal, Dict
 
@@ -77,6 +76,13 @@ class Result(DataClassJsonMixin):
                                for column_result in self.results])
 
 
+def is_valid_objectid(oid: str) -> bool:
+    """
+    Check if a given string is a valid MongoDB ObjectId (24-character hex).
+    """
+    return bool(re.fullmatch(r"^[0-9a-fA-F]{24}$", oid))
+
+
 def is_valid_id(smth_id: Optional[str]) -> bool:
     if smth_id is None:
         return False
@@ -84,8 +90,4 @@ def is_valid_id(smth_id: Optional[str]) -> bool:
     if smth_id.startswith("most-"):
         smth_id = smth_id[5:]
 
-    try:
-        ObjectId(smth_id)
-        return True
-    except:
-        return False
+    return is_valid_objectid(smth_id)
